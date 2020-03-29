db388aadd1503c470dd46fd43efbec82a3880d40
jahnavi1927 <jahnavi142537@gmail.com>
2020-03-29 15:24:56 +0530

    first commit

---
+++ src/main/java/com/maven/TDD_JUNIT/StringRem.java
@@ -0,0 +1,58 @@
+package com.maven.TDD_JUNIT;
+
+public class StringRem {
+
+	public String R(String str)
+    {
+        String res = str;
+        int len = str.length();
+
+        if(len > 2)
+        {
+            if(str.charAt(0) == 'A' && str.charAt(1) == 'A')
+            {
+                res = str.substring(2);
+            }
+            else if(str.charAt(0) == 'A' && str.charAt(1) != 'A')
+            {
+                res = str.substring(1);
+            }
+            else if(str.charAt(0) != 'A' && str.charAt(1) == 'A')
+            {
+                res = str.charAt(0) + str.substring(2);
+            }
+        }
+        else
+        {
+            if(len == 2)
+            {
+                if(str.charAt(0) == 'A' && str.charAt(1) != 'A')
+                {
+                    res = str.charAt(1) + "";
+                }
+                else if(str.charAt(0) != 'A' && str.charAt(1) == 'A')
+                {
+                    res = str.charAt(0) + "";
+                }
+                else
+                {
+                    res = "";
+                }
+            }
+            else
+            {
+                res = "";
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args)
+    {
+        StringRem str = new StringRem();
+        System.out.println(str.R("AAADA"));
+        System.out.println(str.R("ABAB"));
+        System.out.println(str.R("AAAAA"));
+        System.out.println(str.R("CABDC"));
+    }
+}
